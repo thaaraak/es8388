9b7468650c2534302e0a20424082a972e0aeee97
thaaraak <60319319+thaaraak@users.noreply.github.com>
2022-06-10 16:50:52 -0400

    Update streams-i2s-webserver_wav.ino

--- examples/streams-i2s-webserver_wav/streams-i2s-webserver_wav.ino
+++ examples/streams-i2s-webserver_wav/streams-i2s-webserver_wav.ino
@@ -18,24 +18,48 @@ const char *password = "Snitchy43";
 
 AudioWAVServer server(ssid, password);
 
-// Sound Generation
-const int sample_rate = 10000;
-const int channels = 1;
+#include "es8388.h"
+#include "Wire.h"
 
-SineWaveGenerator<int16_t> sineWave;            // Subclass of SoundGenerator with max amplitude of 32000
-GeneratedSoundStream<int16_t> in(sineWave);     // Stream generated from sine wave
-
+uint16_t sample_rate=16000;
+uint16_t channels = 2;
+uint16_t bits_per_sample = 16; // or try with 24 or 32
+I2SStream in;
 
 void setup() {
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial,AudioLogger::Info);
+  AudioLogger::instance().begin(Serial,AudioLogger::Error);
 
   // start server
   server.begin(in, sample_rate, channels);
 
-  // start generation of sound
-  sineWave.begin(channels, sample_rate, N_B4);
-  in.begin();
+  // Input/Output Modes
+  es_dac_output_t output = (es_dac_output_t) ( DAC_OUTPUT_LOUT1 | DAC_OUTPUT_LOUT2 | DAC_OUTPUT_ROUT1 | DAC_OUTPUT_ROUT2 );
+  es_adc_input_t input = ADC_INPUT_LINPUT2_RINPUT2;
+  //  es_adc_input_t input = ADC_INPUT_LINPUT1_RINPUT1;
+
+  TwoWire wire(0);
+  wire.setPins( 33, 32 );
+  
+  es8388 codec;
+  codec.begin( &wire );
+  codec.config( bits_per_sample, output, input, 90 );
+
+  // start I2S in
+  Serial.println("starting I2S...");
+  auto config = in.defaultConfig(RXTX_MODE);
+  config.sample_rate = sample_rate; 
+  config.bits_per_sample = bits_per_sample; 
+  config.channels = channels;
+  config.i2s_format = I2S_STD_FORMAT;
+  config.pin_ws = 25;
+  config.pin_bck = 27;
+  config.pin_data = 26;
+  config.pin_data_rx = 35;
+  //config.fixed_mclk = 0;
+  config.pin_mck = 0;
+  in.begin(config);
+
 }
 
 
